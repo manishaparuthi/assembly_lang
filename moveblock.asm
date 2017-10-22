c5059858a002e942f513ea28745e95717d5df1d6
manishaparuthi <31645396+manishaparuthi@users.noreply.github.com>
2017-10-22 20:48:33 +0530

    move one block of element to another block

---
+++ moveblock.asm
@@ -0,0 +1,43 @@
+;submitted by:manisha paruthi
+;rollno:19
+;prog info
+;move one block of data to another
+data segment
+   block1 db    23h,45h,67h,56h,12h
+   block2 db    5dup(0) 
+
+data ends
+
+code segment
+    assume ds:data , cs:code  
+    
+    start:
+          mov ax,data
+          mov ds,ax         ;populates ds with adress of data segment to data segment register
+          
+          mov si,0
+          mov di,0 
+          
+          
+          lea bx,block1     ;loads effective address of memory loaction named
+                            ; array1 to bx register
+                            
+          lea bp,block2     ;loads effective address of memory loaction named
+                            ; array2 to bp register
+          
+          mov cx,05h        ;number of times the loop to be executed
+     
+     
+     next: 
+          mov al,bx[si]     ;copies the byte content addressed
+                            ; by bx and si registers to al register
+          
+          mov bp[di],al     ;copies the content of al register to memory 
+                            ;location addressed by bp and di registers
+          
+          inc si            ; si=si+1
+                    
+          inc di            ; di=di+1
+          loop next         ;loop condition on cx is checked
+ code ends
+      end start
